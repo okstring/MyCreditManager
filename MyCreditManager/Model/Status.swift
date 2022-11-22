78bc7b39a83c0735d0c483b78afbb90db7616a2c
okstring <milkoi7801@gmail.com>
2022-11-22 20:54:51 +0900

    remove: 불필요한 개행 삭제

--- MyCreditManager/Model/Status.swift
+++ MyCreditManager/Model/Status.swift
@@ -65,17 +65,12 @@ extension SuccessStatus: CustomStringConvertible {
 
 enum ErrorStatus: Error {
     case invalidInput
-
     case invalidRegister
     case registeredStudent(name: String)
-
     case invalidUnregister
     case unregistered(name: String)
-
     case invalidAddCredit
-
     case invalidRemoveCredit
-
     case invalidShowCredit
 }
 
@@ -88,18 +83,14 @@ extension ErrorStatus: LocalizedError {
                 return "입력이 잘못되었습니다. 다시 확인해주세요."
             case .registeredStudent(let student):
                 return "\(student) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다."
-
             case .invalidUnregister:
                 return "입력이 잘못되었습니다. 다시 확인해주세요."
             case .unregistered(let student):
                 return "\(student) 학생을 찾지 못했습니다."
-
             case .invalidAddCredit:
                 return "입력이 잘못되었습니다. 다시 확인해주세요."
-
             case .invalidRemoveCredit:
                 return "입력이 잘못되었습니다. 다시 확인해주세요."
-
             case .invalidShowCredit:
                 return "입력이 잘못되었습니다. 다시 확인해주세요."
         }
